d94b5c06f95d230c20793ec5b0166e8ccb261cc6
H Assaf <64500470+hadi332@users.noreply.github.com>
2024-03-08 22:49:18 +0200

    Update Pyprob-7.py
    Calculate Prime numbers to given limit

--- Pyprob-7.py
+++ Pyprob-7.py
@@ -2,4 +2,16 @@
 #Write a program to print all prime numbers in a given range.
 
 def is_prime(num):
-  
+    if (num <2):
+        return False
+    for i in range(2, int(num**0.5)+1):
+        if num%i ==0:
+                return False
+    return True
+
+def gen_prime_list(num):
+    prime_list = [i for i in range(num+1) if is_prime(i)]
+    return prime_list
+
+number=int (input('Enter your limit: '))
+print("List of prime numbers to ", number, " is ", gen_prime_list(number) )
