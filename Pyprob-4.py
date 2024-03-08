63d180c467684cfebe181e44fa54797e07da92f0
H Assaf <64500470+hadi332@users.noreply.github.com>
2024-03-08 15:37:00 +0200

    Update Pyprob-4.py

--- Pyprob-4.py
+++ Pyprob-4.py
@@ -1,20 +1,11 @@
-#Problem 4: Fibonacci Series 
-#Write a program to generate the Fibonacci series up to a specified number of terms, using recursive method
+#Problem 4: Fibonacci Series
+#Write a program to generate the Fibonacci series up to a specified number of terms.
 
 n = int(input("Enter your number: "))
+fib_series = [0,1]
 
-def fab(num):
-    if num==0:
-        return 0
-    elif (num==1 or num==2):
-        return 1
-    else:
-        return fab(num-1)+fab(num-2)
+for _ in range(2,n+1):
+    next_term = fib_series[-1] + fib_series[-2]
+    fib_series.append(next_term)
 
-result = fab(n)
-
-print("result is ", result)
-#----------------------------------------------------------------------------------------------
-#Write a program to generate the Fibonacci series up to a specified number of terms, using iterative method
-n = int(input("Enter your number: "))
-
+print("Fibonacci Series to order ", n, " is " , fib_series)
