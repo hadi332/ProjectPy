813e34252785e74fa7040a09608e23114c50189d
H Assaf <64500470+hadi332@users.noreply.github.com>
2024-03-08 10:53:48 +0200

    Update Pyprob-1.py
    Basic Problems to practice on Py

--- Pyprob-1.py
+++ Pyprob-1.py
@@ -1,1 +1,7 @@
-import numpy as np
+#Problem 1: Sum of Digits
+#Write a program that calculates the sum of the digits of a given integer
+
+num1=float(input("enter first number:\t"))
+num2=float(input("enter second number:\t"))
+
+print("The result is " + str(num1+num2))
