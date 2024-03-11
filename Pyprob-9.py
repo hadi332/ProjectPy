11e32c5a91284a8c67c32dcd0babe4bd750994a9
H Assaf <64500470+hadi332@users.noreply.github.com>
2024-03-11 14:09:14 +0200

    Update Pyprob-9.py

--- Pyprob-9.py
+++ Pyprob-9.py
@@ -1,5 +1,12 @@
 #Problem 9: List Manipulation
 #Write a program that removes duplicates from a list and sorts it in ascending order.
 
+list_e =[]
 
+words= input("Enter your list: ")
 
+for word in words:
+    if word not in list_e:
+        list_e.append(word)
+list_e.sort()
+print(list_e)
