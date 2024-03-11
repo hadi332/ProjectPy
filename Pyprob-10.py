2b055b3cc28bd5317372027702c7b257bf1a0d59
H Assaf <64500470+hadi332@users.noreply.github.com>
2024-03-11 15:49:13 +0200

    Create Pyprob-10.py

---
+++ Pyprob-10.py
@@ -0,0 +1,8 @@
+ #Problem 10: Caesar Cipher
+#Implement a basic Caesar Cipher encryption and decryption program.
+
+all='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+txt= input("Enter your text: ").upper()
+
+def encrypt(text):
