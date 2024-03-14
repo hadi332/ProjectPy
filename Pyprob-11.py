4527ce49700626b9bc07561e14da57726fab9fba
H Assaf <64500470+hadi332@users.noreply.github.com>
2024-03-14 09:43:33 +0200

    Update Pyprob-11.py

--- Pyprob-11.py
+++ Pyprob-11.py
@@ -1,3 +1,18 @@
-#Problem 11: Decimal to Binary
-#Write a program that converts a decimal number to its binary representation.
+#Problem 11: Integer to Binary
+#Write a program that converts an integer number to its binary representation.
 
+def int_to_bin(num):
+    if num == 0:
+        return '0'
+    
+    binary_num = ''
+    while num > 0:
+        remainder = num % 2
+        binary_num = str(remainder) + binary_num
+        num //= 2 #floor division; which means that it will give you the floor not the nearest value (13//2 = 6 not 7)
+    
+    return binary_num
+
+int_num = int(input("Enter an integer number: "))
+binary_num = int_to_bin(int_num)
+print("Binary representation:", binary_num)
