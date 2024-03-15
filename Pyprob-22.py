61c82906f7c7b366062e70a0a3f6f5d130bb71b5
H Assaf <64500470+hadi332@users.noreply.github.com>
2024-03-15 15:54:03 +0200

    Create Pyprob-22.py

---
+++ Pyprob-22.py
@@ -0,0 +1,7 @@
+#Problem 22: Common Elements
+#Write a function that returns the common elements between two lists.
+
+def commons(list1, list2): 
+    common =[]
+    for l in range(len(list1)):
+        
