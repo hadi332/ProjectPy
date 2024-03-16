0fa15902f7fef29ab844264d8bc4c821ff62cabe
H Assaf <64500470+hadi332@users.noreply.github.com>
2024-03-16 14:37:30 +0200

    Update Pyprob-10.py

--- Pyprob-10.py
+++ Pyprob-10.py
@@ -5,31 +5,31 @@ all='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 txt= input("Enter your text: ").upper()
 
-def caesar_encrypt(text, shift):
+def caesar_encrypt(text):
     encrypted_text = ""
     base = ord('A')
     for char in text:
         if char.isalpha():
-            shifted = (ord(char) - base - shift)%26
+            shifted = (ord(char) - base - 3)%26
             encrypted_text += all[shifted]
         else:
             encrypted_text += char
     return encrypted_text
 
 
-def caesar_decrypt(text, shift):
+def caesar_decrypt(text):
     decrypted_text = ""
     base = ord('A')
     for char in text:
         if char.isalpha():
-            shifted = (ord(char) - base + shift)%26
+            shifted = (ord(char) - base + 3)%26
             decrypted_text += all[shifted]
         else:
             decrypted_text += char
     return decrypted_text
 
 print(txt)
-enc = caesar_encrypt(txt,3)
-dec = caesar_decrypt(enc,3)
+enc = caesar_encrypt(txt)
+dec = caesar_decrypt(enc)
 print(enc)
 print(dec)
