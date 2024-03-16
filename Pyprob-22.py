e84bd4e6e41ac046e0f9514c59de1df450816833
H Assaf <64500470+hadi332@users.noreply.github.com>
2024-03-16 14:31:08 +0200

    Update Pyprob-22.py

--- Pyprob-22.py
+++ Pyprob-22.py
@@ -4,4 +4,15 @@
 def commons(list1, list2): 
     common =[]
     for l in range(len(list1)):
-        
+        for j in range(len(list2)):
+            if list1[l]==list2[j]:
+                common.append(list2[j])
+                     
+    return  common
+
+list_one = [1,3,4,88,5,6,77,8,9]
+list_two = [11,88,44,5,8,9,65547]
+
+result = commons(list_one,list_two)
+
+print('The commons are ', result)
